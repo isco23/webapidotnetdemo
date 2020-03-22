cd0e2bd54692b9ec6307c720c711c9018b46cd12
isco23 <isco23@users.noreply.github.com>
2020-03-22 21:44:26 +0630

    Add DataAccess Project

---
+++ WebApiDotNetDemo/WebApiDotNetDemo/Controllers/ProductsController.cs
@@ -0,0 +1,30 @@
+﻿using DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+
+namespace WebApiDotNetDemo.Controllers
+{
+    public class ProductsController : ApiController
+    {
+        public IEnumerable<Product> Get()
+        {
+            using(DemoEntities demo = new DemoEntities())
+            {
+                return demo.Products.ToList();
+            }
+        }
+
+        public Product Get(int Id)
+        {
+            using (DemoEntities demo = new DemoEntities())
+            {
+                return demo.Products.FirstOrDefault(x => x.Id == Id);
+            }
+        }
+    }
+}
